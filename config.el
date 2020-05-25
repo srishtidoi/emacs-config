12790b9f47651cc9e0aaf692854e96e68c4d317a
TEC <tec@tecosaur.com>
2020-05-25 17:02:16 +0800

    Mu4e: Show the mail modeline icon more reliably

--- config.el
+++ config.el
@@ -628,10 +628,14 @@ clicked."
 (use-package! mu4e-alert
   :after mu4e
   :config
+  (mu4e-alert-enable-mode-line-display)
+  (mu4e-alert-enable-notifications)
+  (setq mu4e-alert-email-notification-types '(subject count))
+  (mu4e-alert-set-default-style 'libnotify)
   (setq mu4e-alert-icon "/usr/share/icons/Papirus/64x64/apps/evolution.svg")
   (defun mu4e-alert-iconised-modeline-formatter (mail-count)
     "Formatter used to get the string to be displayed in the mode-line, using all-the-icons.
-  MAIL-COUNT is the count of mails for which the string is to displayed"
+MAIL-COUNT is the count of mails for which the string is to displayed"
     (when (not (zerop mail-count))
       (concat " "
               (propertize
@@ -650,11 +654,7 @@ clicked."
                                    (mouse-1 . mu4e-alert-view-unread-mails)
                                    (mouse-2 . mu4e-alert-view-unread-mails)
                                    (mouse-3 . mu4e-alert-view-unread-mails))))))
-  (setq mu4e-alert-modeline-formatter #'mu4e-alert-iconised-modeline-formatter)
-  (mu4e-alert-enable-mode-line-display)
-  (setq mu4e-alert-email-notification-types '(subject))
-  (mu4e-alert-set-default-style 'libnotify)
-  (mu4e-alert-enable-notifications))
+  (setq mu4e-alert-modeline-formatter #'mu4e-alert-iconised-modeline-formatter))
 ;; Getting notified:1 ends here
 
 ;; [[file:~/.config/doom/config.org::*Org%20Msg][Org Msg:1]]
