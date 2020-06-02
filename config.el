c85b3f422fcac73a373db89841d96b242b762984
TEC <tec@tecosaur.com>
2020-06-02 17:56:56 +0800

    Add keycast package

--- config.el
+++ config.el
@@ -243,6 +243,27 @@
 (add-hook 'emacs-startup-hook #'greedily-do-daemon-setup)
 ;; Systemd daemon:3 ends here
 
+;; [[file:~/.config/doom/config.org::*Fun][Fun:8]]
+(use-package! keycast
+  :commands keycast-mode
+  :config
+  (define-minor-mode keycast-mode
+    "Show current command and its key binding in the mode line."
+    :global t
+    (if keycast-mode
+        (progn
+          (add-hook 'pre-command-hook 'keycast-mode-line-update t)
+          (add-to-list 'global-mode-string '("" mode-line-keycast " ")))
+      (remove-hook 'pre-command-hook 'keycast-mode-line-update)
+      (setq global-mode-string (remove '("" mode-line-keycast " ") global-mode-string))))
+  (custom-set-faces!
+    '(keycast-command :inherit doom-modeline-debug
+                      :height 0.9)
+    '(keycast-key :inherit custom-modified
+                  :height 1.1
+                  :weight bold)))
+;; Fun:8 ends here
+
 ;; [[file:~/.config/doom/config.org::*Abbrev mode][Abbrev mode:1]]
 (use-package abbrev
   :init
